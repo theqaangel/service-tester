216a24ac280e43548751ed63a7ef999ea146b93f
Angel Tsvetkov <you@example.com>
2015-03-31 11:06:00 +0300

    initial

---
+++ setup.js
@@ -0,0 +1,17 @@
+var mocha = require("mocha/mocha.js");
+
+// Use "tdd" interface.  This is a shortcut to setting the interface;
+// any other options must be passed via an object.
+mocha.setup('tdd');
+
+// This is equivalent to the above.
+mocha.setup({
+  ui: 'tdd'
+});
+
+// Use "tdd" interface, ignore leaks, and force all tests to be asynchronous
+mocha.setup({
+  ui: 'tdd',
+  ignoreLeaks: true,
+  asyncOnly: true
+});
